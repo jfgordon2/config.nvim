62f641d7c0a63b3f11351e3878e308f0e7a62e8b
Jeff Gordon <jeffrey.gordon@optum.com>
2025-01-18 12:28:48 -0600

    plugin updates

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -33,6 +33,8 @@ return { -- Autocompletion
     --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-buffer',
     {
       'zbirenbaum/copilot-cmp',
       opts = {},
