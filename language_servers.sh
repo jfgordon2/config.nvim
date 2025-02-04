38b58f4f4072315d1cede8ea5c95900b6437aeee
Jeff Gordon <jeffrey.gordon@optum.com>
2025-02-03 22:56:59 -0600

    updates

--- language_servers.sh
+++ language_servers.sh
@@ -1,32 +1,37 @@
 #! /bin/bash
 
+brew install tree-sitter --HEAD
 brew install neovim --HEAD
-brew install ripgrep
+
+brew install actionlint
+brew install codespell
+brew install --cask codeql
 brew install fd
 brew install fzf
-brew install codespell
+brew install jsonlint
 brew install lazygit
 brew install lua
 brew install luacheck
 brew install luajit
 brew install luarocks
+brew install luv
+brew install markdownlint-cli
+brew install markdownlint-cli2
+brew install ripgrep
+brew install ruff
 brew install shellcheck
+brew install shfmt
+brew install sqlfluff
 brew install taplo
 brew install tflint
 brew install tfsec
 brew install yamllint
-brew install ruff
-brew install jsonlint
-brew install vale
-brew install markdownlint-cli
-brew install markdownlint-cli2
-brew install sqlfluff
-brew install --cask codeql
-npm install -g @microsoft/compose-language-service
-npm install -g dockerfile-language-server-nodejs
-npm install -g @ansible/ansible-language-server
-npm install -g awk-language-server
-npm install -g bash-language-server
-npm install -g vscode-langservers-extracted
-npm install -g gh-actions-language-server
-npm install -g @biomejs/biome
+
+# npm install -g @ansible/ansible-language-server
+# npm install -g awk-language-server
+# npm install -g bash-language-server
+# npm install -g @biomejs/biome
+# npm install -g dockerfile-language-server-nodejs
+# npm install -g gh-actions-language-server
+# npm install -g @microsoft/compose-language-service
+# npm install -g vscode-langservers-extracted
